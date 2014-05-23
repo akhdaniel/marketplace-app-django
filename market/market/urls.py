7b51cdd5eb6c40d19fe01bc1c5fb3bb484ea379f
Jawwad Siddiqui <hi@jawwad.me>
2014-05-23 10:51:48 -0400

    New Project: Market App

---
+++ market/market/urls.py
@@ -0,0 +1,17 @@
+#Import Settings to serve static files
+from django.conf import settings
+
+
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'market.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    (r'^static/(?P<path>. *)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    (r'^media/(?P<path>. *)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^admin/', include(admin.site.urls)),
+)
